0fffb809e5ec26ed3e8d752fd3a716a0496e52bf
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-09-10 23:37:12 +0300

    Add more tests

---
+++ tests/src/RecordsTraits/HasStatus/RecordTraitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ByTIC\Models\SmartProperties\Tests\RecordsTraits\HasStatus;
+
+use ByTIC\Models\SmartProperties\Properties\AbstractProperty\Generic;
+use ByTIC\Models\SmartProperties\Tests\Fixtures\RecordsTraits\HasStatus\Record;
+use ByTIC\Models\SmartProperties\Tests\Fixtures\RecordsTraits\HasStatus\Records;
+use ByTIC\Models\SmartProperties\Tests\AbstractTest;
+
+/**
+ * Class TraitsTest
+ * @package ByTIC\Models\SmartProperties\Tests\RecordsTraits\HasStatus
+ */
+class RecordTraitTest extends AbstractTest
+{
+    /**
+     * @var Record
+     */
+    private $object;
+
+    public function testGetStatusWithoutValue()
+    {
+        $status = $this->object->getStatus();
+        self::assertInstanceOf(Generic::class, $status);
+        self::assertSame('allocated', $status->getName());
+    }
+
+    public function testGetStatusWithValue()
+    {
+        $this->object->status = 'applicant';
+
+        $status = $this->object->getStatus();
+        self::assertInstanceOf(Generic::class, $status);
+        self::assertSame('applicant', $status->getName());
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->object = new Record();
+
+        $manager = new Records();
+        $this->object->setManager($manager);
+    }
+}
---
+++ tests/src/RecordsTraits/HasStatus/RecordsTraitTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace ByTIC\Models\SmartProperties\Tests\RecordsTraits\HasStatus;
+
+use ByTIC\Models\SmartProperties\Tests\Fixtures\RecordsTraits\HasStatus\Records;
+use ByTIC\Models\SmartProperties\Tests\Fixtures\RecordsTraits\HasStatus\Statuses\Allocated;
+use ByTIC\Models\SmartProperties\Tests\Fixtures\RecordsTraits\HasStatus\Statuses\Applicant;
+use ByTIC\Models\SmartProperties\Tests\AbstractTest;
+
+/**
+ * Class TraitsTest
+ * @package ByTIC\Models\SmartProperties\Tests\Payments\Methods
+ */
+class RecordsTraitTest extends AbstractTest
+{
+    /**
+     * @var Records
+     */
+    private $object;
+
+    public function testGetSmartPropertiesDefinitions()
+    {
+        $definitions = $this->object->getSmartPropertiesDefinitions();
+        self::assertCount(1, $definitions);
+    }
+
+    public function testGetStatusesDirectory()
+    {
+        $directory = $this->object->getStatusesDirectory();
+
+        self::assertStringEndsWith(
+            str_replace('/', DIRECTORY_SEPARATOR, 'RecordsTraits/HasStatus/Statuses'),
+            $directory
+        );
+    }
+
+    public function testGetStatuses()
+    {
+        $statuses = $this->object->getStatuses();
+        self::assertCount(2, $statuses);
+        self::assertInstanceOf(Allocated::class, $statuses['allocated']);
+        self::assertInstanceOf(Applicant::class, $statuses['applicant']);
+    }
+
+    public function testGetStatusProperty()
+    {
+        $values = $this->object->getStatusProperty('name');
+        self::assertSame(['allocated', 'applicant'], $values);
+    }
+
+    public function testGetStatus()
+    {
+        $status = $this->object->getStatus('allocated');
+        self::assertInstanceOf(Allocated::class, $status);
+    }
+
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->object = new Records();
+    }
+}
