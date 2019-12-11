10361ce0d11344615316c05e17aa85a6c84dd3fc
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-12-11 11:21:05 +0200

    fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+use Nip\Container\Container;
+
 define('PROJECT_BASE_PATH', __DIR__ . '/..');
 define('TEST_BASE_PATH', __DIR__);
 define('TEST_FIXTURE_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'fixtures');
@@ -9,3 +11,7 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+$container = new Container();
+$container->set('inflector', new \Nip\Inflector\Inflector());
+Container::setInstance($container);
